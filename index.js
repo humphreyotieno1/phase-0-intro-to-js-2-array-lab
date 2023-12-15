eabf54b1e38af86330900fa41ef35751832aaa8d
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-16 00:47:06 +0300

    Arrays and methods

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat (name) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveFirstCat (name) {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat (name) {
+    cats.pop();
+}
+
+function appendCat(name) {
+    //let newCats = cats.slice();
+    //newCats.push('Broom');
+    return [...cats, 'Broom'];
+}
+
+function prependCat (name) {
+    let x = [...cats];
+    x.unshift('Arnold');
+    return x;
+   // newCats.unshift('Arnold');
+   // return newCats;
+}
+
+function removeFirstCat (name) {
+    return cats.slice(1);
+}
+
+function removeLastCat (name) {
+    return cats.slice(0, cats.length - 1);
+}
